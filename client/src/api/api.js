429ed116afb2467db0c663932e1ab573ad31cbd4
unknown <abhishekydv2408@gmail.com>
2026-05-02 00:25:22 +0530

    update server

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/api' })
+const api = axios.create({ baseURL: import.meta.env.VITE_API_URL || 'https://team-task-manager-production-backend.up.railway.app/api' })
 
 // Attach token to every request
 api.interceptors.request.use((config) => {
